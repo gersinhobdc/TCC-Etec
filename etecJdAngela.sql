eda2e6c50aa9e54eab1bd3b898ec4d6fc10a516d
gersinhobdc <gerson.graziani@gmail.com>
2024-03-15 10:48:26 -0300

    Tabelas feitas do BD

--- etecJdAngela.sql
+++ etecJdAngela.sql
@@ -18,7 +18,32 @@ email varchar(255) not null,
 senha varchar(255) not null,
 PRIMARY KEY (id));
 
+CREATE TABLE labs(
+id int not null AUTO_INCREMENT,
+nmr_Sala int not null,
+nivel_pc varchar(255) not null,
+PRIMARY KEY (id));
 
+CREATE TABLE patrimonio(
+id int not null AUTO_INCREMENT,
+nome_prod varchar(255) not null,
+quant int not null,
+descricao varchar(255),
+data_compra date,
+fornecedor varchar(255), 
+PRIMARY KEY (id));
+
+CREATE TABLE permissao(
+id int not null AUTO_INCREMENT,
+adm boolean not null,
+email varchar(255) not null,
+PRIMARY KEY (id));
+
+CREATE TABLE disciplinas(
+id int not null AUTO_INCREMENT,
+nome_disc varchar(255) not null,
+prof_disc varchar(255) not null,
+PRIMARY KEY (id));
 
 
 INSERT INTO `login` (`email`, `senha`) VALUES ('admin', 'admin'); 
